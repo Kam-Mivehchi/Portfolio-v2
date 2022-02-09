77e431f6ce1d45868e723483c54470d0fe6813d9
Kamyar Mivehchi <kammivehchi@gmail.com>
2022-02-09 11:31:05 -0800

    centered avatar on small screens

---
+++ index.js
@@ -0,0 +1,37 @@
+const title = $('#title');
+const description = $('#desc');
+const gitLink = $("#git-link")
+const siteLink = $('#deploy-link')
+
+
+const projectData = [
+    {
+        "title": "Type{Scripts}",
+        "desc": `Type{Scripts} is a typing game for all developers, new and experienced alike;
+                        where users are able to see their typing skills when typing code.We noticed that a lot of the
+                        typing quizes or games didn't offer developer modes where you can type code instead of random
+                        words and sentences and sought to provide a solution in the form of Typescripts.This being our
+                        very first full stack development project, we learned a lot about technical skills and social
+                        skills when working in a team.`,
+        "language": "JavaScript, HTML, CSS"
+    }
+    {
+        "title": "Workout Wizard",
+        "desc": "Application where the user is able to find a workout specifically tocertainmuscle groups.If the user selects a muscle group, application will provide a list ofworkoutsand videos.",
+        "language": "JavaScript, HTML, CSS"
+    }
+    // {
+    //     "title": ,
+    //     "desc": ,
+    //     "language": "JavaScript, HTML, CSS"
+    // }
+    // {
+    //     "title": ,
+    //     "desc": ,
+    //     "language": "JavaScript, HTML, CSS"
+    // }
+]
+
+$('#card-text').each((projectData, function(i,val))=>{
+
+})
